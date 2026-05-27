e80d0bb154f18c93d83040e58ef0a8833bea4f0b
Leandro Almeida <vempari@gmail.com>
2026-05-27 11:34:44 -0300

    update to sync paths usage

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import path from 'path'
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,18 +15,6 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
+    tsconfigPaths(),
   ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src/'),
-      assets: `${path.resolve(__dirname, './src/assets/')}`,
-      config: `${path.resolve(__dirname, './src/config/')}`,
-      constants: `${path.resolve(__dirname, './src/constants/')}`,
-      core: `${path.resolve(__dirname, './src/core/')}`,
-      layout: `${path.resolve(__dirname, './src/layout/')}`,
-      pages: path.resolve(__dirname, './src/pages'),
-      lib: `${path.resolve(__dirname, './src/lib/')}`,
-      types: `${path.resolve(__dirname, './src/types')}`,
-    },
-  },
 })
